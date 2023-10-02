ddde88fbd0302a9169ebc03b403626f3215b4892
Pankaj Lamba <112269355+cspankajlamba@users.noreply.github.com>
2023-10-02 10:33:24 +0530

    Update MergeArrays.js

--- MergeArrays.js
+++ MergeArrays.js
@@ -4,11 +4,11 @@ const cars = ["car1", "car2"];
         const buses = ["bus1", "bus2"];
         document.write(buses);
 
-        // method 1 : concat
+        // Method 1 : Concatenation Method
         const comb1 = [].concat(cars, buses);
         document.write("<br>");
         document.write(comb1);
         document.write("<br>");
-        //method 2 : Spread
+        //Method 2 : Spread Method
         const comb2 = [...cars, ...buses];
         document.write(comb2);
